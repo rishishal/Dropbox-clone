5ee7531bf243524c529ee572297cb4f02074ce8e
rishishal <rishisahilcomputer@gmail.com>
2024-01-19 23:08:19 +0530

    Project Completed

---
+++ src/app/download/[userId]/file/[fileId]/_components/FileItems.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Download } from "lucide-react";
+import Image from "next/image";
+import { FileType } from "@/typing";
+import { ReactElement } from "react";
+
+interface fileItemProps {
+  file: FileType;
+}
+
+const FileItems: React.FC<fileItemProps> = ({ file }): ReactElement => {
+  return (
+    file && (
+      <div>
+        <div className='p-5 rounded-md bg-white flex flex-col items-center'>
+          <div className='text-center flex-col gap-3 items-center flex'>
+            <h2 className='text-[20px] text-gray-600'>
+              <strong className='text-blue-600'>{file?.fullName}</strong> Shared
+              the file with You
+            </h2>
+            <h2 className='text-[10px] text-gray-400'>
+              Find File details below
+            </h2>
+            <Image
+              src='/download-file.gif'
+              alt='download Gif'
+              width={150}
+              height={150}
+              className='w-[150px] h-[150px] p-5'
+            />
+            <h2 className='text-gray-500 text-[15px]'>
+              {file?.filename} ⚡ {file?.type} ⚡ {file?.size} Byets
+            </h2>
+          </div>
+
+          <button className='flex gap-2 p-2 bg-blue-600 text-white rounded-full w-full items-center hover:bg-blue-600 text-[14px] mt-5 text-center justify-center'>
+            <Download className='h-4 w-4' />
+            Download
+          </button>
+          <h2 className='text-gray-400 text-[12px]'>
+            *Term and Conditions apply
+          </h2>
+        </div>
+      </div>
+    )
+  );
+};
+export default FileItems;
--- src/app/download/[userId]/file/[fileId]/page.tsx
+++ src/app/download/[userId]/file/[fileId]/page.tsx
@@ -1,19 +1,51 @@
-const page = ({
-  params,
-}: {
+"use client";
+
+import { db } from "@/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { FileType } from "@/typing";
+import FileItems from "./_components/FileItems";
+import Link from "next/link";
+
+interface PageParams {
   params: {
     userId: string;
     fileId: string;
   };
-}) => {
+  file: FileType;
+}
+
+const page = ({ params }: PageParams) => {
   const userId = params.userId;
   const fileId = params.fileId;
 
+  const [file, setFile] = useState<FileType | null>();
+
+  useEffect(() => {
+    userId && fileId && getFileInfo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId, fileId]);
+
+  const getFileInfo = async () => {
+    const docRef = doc(db, "users", userId, "files", fileId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      // console.log("Document data:", docSnap.data());
+      setFile(docSnap.data() as FileType);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
   return (
-    <div>
-      <h1>
-        UserId {params.userId} and FileId{params.fileId}
-      </h1>
+    <div className='bg-gray-100 h-screen w-full flex justify-center items-center flex-col gap-4'>
+      <Link href='/'>
+        <Image src='/dropbox.svg' alt='LOGO' width={60} height={60} />
+      </Link>
+      <FileItems file={file!} />
     </div>
   );
 };
